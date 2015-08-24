a37a33e37284cbe747e96127c77c102223ae208b
jacobkoikkara <jacob.koikkara@gmail.com>
2015-08-24 17:23:51 -0500

    Adding splash screen adding color and styles for splash screen text.

---
+++ app/src/main/java/com/jcb/enunciate/activity/EnBaseActivity.java
@@ -0,0 +1,15 @@
+package com.jcb.enunciate.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jacobkoikkara on 8/24/15.
+ */
+public class EnBaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/jcb/enunciate/activity/EnMainActivity.java
@@ -0,0 +1,20 @@
+package com.jcb.enunciate.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by jacobkoikkara on 8/24/15.
+ */
+public class EnMainActivity extends EnBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+
+}
+
